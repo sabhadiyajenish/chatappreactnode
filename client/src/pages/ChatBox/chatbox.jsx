d677cb2c0bda9b9fbb4deefe98c8497b20464ada
jenish <sabhadiyajenish83@gmail.com>
2024-07-04 11:42:51 +0530

    remove this

--- client/src/pages/ChatBox/chatbox.jsx
+++ client/src/pages/ChatBox/chatbox.jsx
@@ -1461,7 +1461,7 @@ const Chatbox = () => {
                 onClick={UploadFileOnCloud}
                 className="text-white mt-3 w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
-                {loadingForUploadImage ? "sending... " : "Send"}
+                {loadingForUploadImage ? "sending.... " : "Send"}
               </button>
             </div>
           )}
