b5431b8eee906f0e07a8367fca2db111ec9ba660
sabhadiya jenish <sabhadiyajenish83@gmail.com>
2025-02-17 15:50:56 +0530

    added

--- client/src/utils/constant.jsx
+++ client/src/utils/constant.jsx
@@ -3,12 +3,12 @@ import Cookies from "js-cookie";
 const apiUrl = import.meta.env.VITE_REACT_APP_SERVICE_URL;
 // console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", apiUrl);
 
-// export const LOCAL_PATH = "https://chatappreactnode-1.onrender.com/api/v1";
-// export const SOCKET_URL = "https://chatappreactnode-1.onrender.com";
+export const LOCAL_PATH = "https://chatappreactnode-1.onrender.com/api/v1";
+export const SOCKET_URL = "https://chatappreactnode-1.onrender.com";
 // export const LOCAL_PATH = "http://localhost:5000/api/v1";
 // export const SOCKET_URL = "http://localhost:5000";
-export const LOCAL_PATH = "http://192.168.29.162:5000/api/v1";
-export const SOCKET_URL = "http://192.168.29.162:5000";
+// export const LOCAL_PATH = "http://192.168.29.162:5000/api/v1";
+// export const SOCKET_URL = "http://192.168.29.162:5000";
 export const ENCRYPTION_KEY =
   "382efde76cc9ae91666c85c6cae72536ae45080ecaa74cbd46a8d9377180a7c4";
 
